d6e96f39200509ba0cd98556aa49ea2868206fec
Niklas Pietiläinen <Niklas Pietilinen>
2016-07-21 15:52:36 +0300

    Camera changes

---
+++ Gravoyager/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+
+
+    public Transform player;
+    public Vector3 offset;
+
+
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
+
+        
+
+        
+
+    }
+}
