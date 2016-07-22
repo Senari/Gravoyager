b524c73cc1e0f67bf67cd19c244a1601e25fa055
Vuorio Leo <olevu002@edu.mamk.fi>
2016-07-22 11:00:02 +0300

    Narrative

---
+++ Gravoyager/Assets/Scripts/narrativeScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class narrativeScript : MonoBehaviour
+{
+    public Canvas storyMenu;
+    public Button startText;
+    public Button exitText;
+
+   
+
+    public void Startlevel()
+    {
+        Application.LoadLevel(2);
+    }
+    public void ExitGame()
+
+    {
+        Application.LoadLevel(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
