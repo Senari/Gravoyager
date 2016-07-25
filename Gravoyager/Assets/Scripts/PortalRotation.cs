1c78133156fac023cb9cf381d406e4058a5afd67
Krutikov Alexey <oalkr001@edu.mamk.fi>
2016-07-25 12:26:52 +0300

    - Updated gravity script

---
+++ Gravoyager/Assets/Scripts/PortalRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalRotation : MonoBehaviour {
+
+    public float speed = 10f;
+   
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+    }
+}
