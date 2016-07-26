6f59cfde273f0d861f22b9ce8204ed2224528d11
Niklas Pietiläinen <Niklas Pietilinen>
2016-07-27 00:43:25 +0300

    Thruster changes

--- Gravoyager/Assets/Scripts/ThrusterFlame.cs
+++ Gravoyager/Assets/Scripts/ThrusterFlame.cs
@@ -5,7 +5,9 @@ public class ThrusterFlame : MonoBehaviour
 {	
 	public GameObject forwardFlame;
 	public GameObject reverseFlame;
-	//private bool ifEnoughFuel = PlayerScript.FuelConsumption(); //I want to have an access to that method in PlayerScript class
+    //private bool ifEnoughFuel = PlayerScript.FuelConsumption(); //I want to have an access to that method in PlayerScript class
+
+    public float fuel;
 
 	void Start () 
 	{
@@ -14,16 +16,21 @@ public class ThrusterFlame : MonoBehaviour
 		
 	void FixedUpdate() 
 	{
-		//For main thruster
-		if (Input.GetKey (KeyCode.UpArrow)/* && ((ifEnoughFuel = true)*/) //And if THAT method in another class shows there is some fuel in tanks
-			forwardFlame.GetComponent<SpriteRenderer>().enabled = true;
-		else	
-			forwardFlame.GetComponent<SpriteRenderer>().enabled = false;
 
-		//And for reverse thruster
-		if (Input.GetKey (KeyCode.DownArrow)/* && (ifEnoughFuel = true)*/)
-			reverseFlame.GetComponent<SpriteRenderer>().enabled = true;
-		else			
-			reverseFlame.GetComponent<SpriteRenderer>().enabled = false;
+        fuel = PlayerScript.S.ReturnFuel();
+
+        
+            //For main thruster
+            if (Input.GetKey(KeyCode.UpArrow) && fuel >= 0) //And if THAT method in another class shows there is some fuel in tanks
+                forwardFlame.GetComponent<SpriteRenderer>().enabled = true;
+            else
+                forwardFlame.GetComponent<SpriteRenderer>().enabled = false;
+
+            //And for reverse thruster
+            if (Input.GetKey(KeyCode.DownArrow) && fuel >= 0)
+                reverseFlame.GetComponent<SpriteRenderer>().enabled = true;
+            else
+                reverseFlame.GetComponent<SpriteRenderer>().enabled = false;
+        
 	}
 }
