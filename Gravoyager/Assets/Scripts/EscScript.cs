a68396e507d229c99abd8b9d57631b509c1c9229
Vuorio Leo <olevu002@edu.mamk.fi>
2016-07-26 12:05:15 +0300

    R to restart, esc to pause

--- Gravoyager/Assets/Scripts/EscScript.cs
+++ Gravoyager/Assets/Scripts/EscScript.cs
@@ -1,31 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class EscScript : MonoBehaviour {
+public class EscScript : MonoBehaviour
+{
+    public Canvas escMenu;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public Button restart;
+
+    public Button jatka;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+
+
+    void Update()
+    {
+
         if (Input.GetKey(KeyCode.Escape))
         {
+
+
             if (Time.timeScale == 0)
             {
+
+
                 Time.timeScale = 1;
             }
             else {
+
                 Time.timeScale = 0;
+
             }
+
+
+
+
         }
-    }
-    void OnGUI()
-    {
-        if (Time.timeScale == 0)
+
+        if (Input.GetKey(KeyCode.R))
         {
-            //Display your gui.        
+
+            Application.LoadLevel(3);
         }
+
     }
 }
