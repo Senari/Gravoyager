d0ce710a261bdfbf5020b755ef43bb0e4841403f
Pietiläinen Niklas <onipi007@edu.mamk.fi>
2016-07-27 12:32:43 +0300

    Pointer changes and stuff

---
+++ Gravoyager/Assets/Scripts/Pointer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pointer : MonoBehaviour {
+
+    public Transform target;
+
+    void Update()
+    {
+        Vector3 difference = target.position - transform.position;
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+    }
+}
