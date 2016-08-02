01746f80b69942b6640c8c28218031057d9d36e6
Niklas Pietiläinen <Niklas Pietilinen>
2016-08-02 16:37:24 +0300

    It should be fine now?

--- Gravoyager/Assets/Scripts/PlayerScript.cs
+++ Gravoyager/Assets/Scripts/PlayerScript.cs
@@ -19,7 +19,10 @@ public class PlayerScript : MonoBehaviour
     public float boostCooldown = 1;
     private Rigidbody2D rigidbody;
 
-    
+    //SpeedMeter
+    public Text speedMeter;
+
+    private Vector2 oldPosition;
 
     //Fuel
     public float maxFuel = 500f;//Tank's capacity
@@ -28,24 +31,29 @@ public class PlayerScript : MonoBehaviour
     public float refuelingSpeed = 8f;
 
     //Cargo
-    public float maxCargo = 100f;
-    private float currentCargo;
-	public float miningSpeed = 5f;
+    public float maxCargo;
+    public float currentCargo;
 
     //Speed
     private float distancePerTime;//Player ship's speed
     public float crashOnSpeed;//Destroy the ship when crash speed is more than...
-	private Vector2 oldPosition;
-	private Vector2 velocityVector;
 
     //trajectory
-    public int trajectoryDots;
+    public int trajectoryDots = 0;
     private float dotSpread;
     public GameObject dot;
-	private GameObject[] futurePlayer;
+
     public Slider FuelSlider;
 
-    void Start()
+	//Death effects
+	private Animator explosion;
+	public AudioClip explosionSound;
+	public AudioSource explosionSource;
+	[HideInInspector]
+	public bool Alive = true;
+
+
+	void Start()
     {
         S = this;
 
@@ -53,37 +61,49 @@ public class PlayerScript : MonoBehaviour
         oldPosition = this.transform.position;
         currentFuel = maxFuel;//Ship's tank is loaded full at start
         currentCargo = 0;
+        speedMeter.text = "0";
+
+		explosion = GetComponent<Animator> (); // Player death animation
+		explosionSound = GetComponent<AudioClip>();
     }
 
     void FixedUpdate()
     {
         float consumptionForward = 1;//This one uses the same value as in fuelConsumptionSpeed for forward thrusters
         float consumptionReverse = ReverseMovementSpeed / MovementSpeed;//There we get coefficient which is less (or more) to change ConsumptionSpeed for reverse thruster
-        
-        //Movement
-        if (Input.GetKey(KeyCode.RightArrow))
-            transform.Rotate(-Vector3.forward * RotateSpeed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.LeftArrow))
-            transform.Rotate(Vector3.forward * RotateSpeed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            if (FuelConsumption(consumptionForward))
-            {
-                rigidbody.AddForce(transform.up * MovementSpeed);
-            }
-        }
 
-        if (Input.GetKey(KeyCode.DownArrow))
+
+		if (Alive)
+		{
+			//Movement
+			if (Input.GetKey(KeyCode.RightArrow))
+				transform.Rotate(-Vector3.forward * RotateSpeed * Time.deltaTime);
+			if (Input.GetKey(KeyCode.LeftArrow))
+				transform.Rotate(Vector3.forward * RotateSpeed * Time.deltaTime);
+			if (Input.GetKey(KeyCode.UpArrow))
+			{
+				if (FuelConsumption(consumptionForward))
+				{
+					rigidbody.AddForce(transform.up * MovementSpeed);
+				}
+			}
+
+			if (Input.GetKey(KeyCode.DownArrow))
+			{
+				if (FuelConsumption(consumptionReverse))
+				{
+					rigidbody.AddForce(transform.up * -ReverseMovementSpeed);
+				}
+			}
+		}
+
+		Vector2 myPosition = this.transform.position;
+        distancePerTime = Vector2.Distance(oldPosition, myPosition);
+        float absoluteSpeed = distancePerTime * 50;
+        if (speedMeter != null)
         {
-            if (FuelConsumption(consumptionReverse))
-            {
-                rigidbody.AddForce(transform.up * -ReverseMovementSpeed);
-            }
+            speedMeter.text = absoluteSpeed.ToString("F2");     //quotes for string
         }
-        Vector2 myPosition = this.transform.position;
-        distancePerTime = Vector2.Distance(oldPosition, myPosition); //float value
-		velocityVector = oldPosition - myPosition; //vector value
-        //print("Absolute Speed: " + distancePerTime * 50);       
 
         //Trajectory
         if ((oldPosition != myPosition) && (trajectoryDots >= 1))
@@ -104,7 +124,8 @@ public class PlayerScript : MonoBehaviour
         //GameArea();
 
         //Slider value
-        FuelSlider.value = currentFuel;        
+        FuelSlider.value = currentFuel;
+        
     }
 
     void GameArea()
@@ -153,20 +174,19 @@ public class PlayerScript : MonoBehaviour
         //For collisions on speed
         float speedCol = collider.gameObject.GetComponent<SpeedsOfObjects>().objectSpeed;//Object's speed
         float relativeSpeed = speedCol * 50 - distancePerTime * 50;//Impact speed. Object's and ship's speeds have to be multiplied by 20 to get m/s format
-        //print("Contact Speed was: " + relativeSpeed);
+        print("Contact Speed was: " + relativeSpeed);
 
         //Relative speed is a difference of ship's and object's speed. It can be negative, that's why we also use OR state
         if ((relativeSpeed >= crashOnSpeed) || (relativeSpeed <= -crashOnSpeed))
         {
-            Destroy(gameObject);
-            Destroyed();
+			
+			Destroyed();
+
         }
 
 
         if (collider.gameObject.tag == "FuelStations")
             Refueling();//If refueling does not work, check if platform has SpeedsOfObjects script attached
-		if (collider.gameObject.name == "Planet")
-			LoadingCargo ();//
     }
 
     public void Refueling()
@@ -174,70 +194,63 @@ public class PlayerScript : MonoBehaviour
         if (currentFuel <= maxFuel)//To avoid overfueling cheat
             currentFuel = currentFuel + (refuelingSpeed * Time.deltaTime);
     }
-
     public float ReturnFuel()
     {
         return currentFuel;
     }
 
-	public void LoadingCargo()
-	{
-		if ((Input.GetKeyDown (KeyCode.C)) && (currentCargo <= maxCargo))
-			currentCargo += (miningSpeed * Time.deltaTime);
-	}
-	public float ReturnCargo()
-	{
-		return currentCargo;
-	}
-
     public void Destroyed()
     {
-        Application.LoadLevel(4);
+		{			
+			// Plays the explosion animation, freezes player movement, and causes the restart screen to load after a delay.
+			//Destroy(gameObject);
+			Alive = false;
+			explosion.enabled = true;
+			rigidbody.constraints = RigidbodyConstraints2D.FreezeAll; 
+			Invoke ("Restart", 1.7f);
+			explosionSource.enabled = true;
+		}
     }
 
     //Trajectory prediction
     public void Trajectory()
-    {	
+    {
         Vector2 pos1 = oldPosition;
-		Vector2 pos2 = this.transform.position;
-		Vector2 gravity;
-		Vector2 mov;
-
+        Vector2 pos2 = this.transform.position;
             for (int d = 0; d < trajectoryDots; d++)
             {
                 GameObject futurePlayer = Instantiate(dot); // New game object for future me
-                //futurePlayer.transform.position = gravity - velocityVector; //Location of future me
+                futurePlayer.transform.position = pos2 +(pos2-pos1); //Location of future me
 
                 //Gravity to attention
                 GameObject[] body = GameObject.FindGameObjectsWithTag("Massed");
                 for (int i = 0; i < body.Length; i++)
                 {
                     // exclude yourself
-				if (/*(body[i] != futurePlayer) &&*/ (body[i] != gameObject))
+                    if (body[i] != gameObject)
                     {
                         Vector2 bodyPos = body[i].transform.position;//Creating an objects to work with Player and Planet
-                        Vector2 playerPos = this.transform.position; //"This" because script is applied to player
+                        Vector2 playerPos = futurePlayer.transform.position; //"This" because script is applied to player
 
-					float distance = Vector2.Distance(bodyPos, playerPos);//Function to find the distance. Vector2 because of 2D dimension.
+                        float distance = Vector2.Distance(bodyPos, playerPos);//Function to find the distance. Vector2 because of 2D dimension.
 
-                        float playerMass = this.GetComponent<Rigidbody2D>().mass;//Taking mass from Rigidbody
+                        float playerMass = futurePlayer.GetComponent<Rigidbody2D>().mass;//Taking mass from Rigidbody
                         float bodyMass = body[i].GetComponent<Rigidbody2D>().mass;
-                        float weight = (bodyMass * playerMass) / (distance * distance);
-                        
-					gravity = body[i].transform.position - this.transform.position;
-					mov = new Vector2((gravity.normalized.x * (weight/playerMass)) + velocityVector.normalized.x * distancePerTime, (gravity.normalized.y* (weight/playerMass)) + velocityVector.normalized.y * distancePerTime);
-						futurePlayer.transform.position = pos2 - mov;
-						
-						//print ("GravitySpeed: ");
-	                 }
-                }		
-			futurePlayer.GetComponent<Renderer>().enabled = true;
-				
-				//futurePlayer.transform.position = 
-                pos1 = this.transform.position;
-				pos2 = futurePlayer.transform.position;
-				Destroy(futurePlayer.gameObject, 0.02f);
 
+
+                        float weight = (bodyMass * playerMass) / (distance * distance);
+                        Vector2 gravity = body[i].transform.position - futurePlayer.transform.position;
+                        futurePlayer.GetComponent<Rigidbody2D>().AddForce(gravity.normalized * weight);
+                    }
+                }
+                //Destroy(futurePlayer.gameObject, 0.02f);
+                pos1 = this.transform.position;
+                pos2 = futurePlayer.transform.position;
             }            
        }//Trajectory prediction
+
+	public void Restart()
+	{
+		Application.LoadLevel (4);
+	}
 }
