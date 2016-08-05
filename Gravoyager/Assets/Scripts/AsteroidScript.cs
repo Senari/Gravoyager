4ede17b5cfcee355af7d3f30865030855cf7f063
Pietiläinen Niklas <onipi007@edu.mamk.fi>
2016-08-05 15:02:23 +0300

    Asteroid fields on first stage

--- Gravoyager/Assets/Scripts/AsteroidScript.cs
+++ Gravoyager/Assets/Scripts/AsteroidScript.cs
@@ -10,18 +10,37 @@ public class AsteroidScript : MonoBehaviour {
     public float movementSpeed;
     public float rotationSpeed;
 
+	public GameObject SpawnPoint;
+
+	int laskuri = 0;
+
+	public int length = 340;
 
 
     // Use this for initialization
     void Start () {
-        for (int i = 0; i < 100; ++i)
+
+	
+
+
+        for (int i = 0; i < 500; ++i)
         {
-            GameObject newAsteroid = (GameObject)Instantiate(asteroidPrefab, Random.insideUnitSphere * fieldRadius, Random.rotation);
+			if (laskuri >= length) {
+
+				laskuri = 0;
+
+			}
+
+
+			GameObject newAsteroid = (GameObject)Instantiate(asteroidPrefab, new Vector3(laskuri + (SpawnPoint.transform.position.x - 170), SpawnPoint.transform.position.y, 0), Quaternion.identity);
             float size = Random.Range(0.01f, 2);
             newAsteroid.transform.localScale = Vector3.one * size;
             // if the asteroid has a rigidbody...
-            newAsteroid.GetComponent<Rigidbody>().velocity = Random.insideUnitSphere * movementSpeed;
-             newAsteroid.GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * rotationSpeed;
+            newAsteroid.GetComponent<Rigidbody2D>().velocity = Random.insideUnitCircle * movementSpeed;
+             //newAsteroid.GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * rotationSpeed;
+
+			laskuri++;
+
         }
     }
 	
