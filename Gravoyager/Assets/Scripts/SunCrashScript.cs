d53f8e70871c7dfcd2ca595bb13b072f053ea556
Niklas Pietiläinen <Niklas Pietilinen>
2016-08-08 15:31:09 +0300

    Some sun fixes

---
+++ Gravoyager/Assets/Scripts/SunCrashScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunCrashScript : MonoBehaviour {
+
+    public static PlayerScript S;
+
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+
+            PlayerScript.S.Destroyed();
+
+        }
+
+
+    }
+}
